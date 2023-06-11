ce3f60f3cbe8d7e13f443eb88b5860d670e5c84c
Mor <morhanania@gmail.com>
2023-06-12 02:19:19 +0300

    styles the sign up

--- app/src/main/java/com/example/wink_android/SignUpActivity.java
+++ app/src/main/java/com/example/wink_android/SignUpActivity.java
@@ -1,26 +1,65 @@
 package com.example.wink_android;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class SignUpActivity extends AppCompatActivity {
 
 
     private EditText usernameEditText;
     private EditText passwordEditText;
-    private String password;
-    private ImageView selectedImageView;
     private EditText confirmEditText;
     private EditText displayEditText;
     private Button uploadButton;
+    private ImageView circleImageView;
+
+    private String password;
+    private String confirm;
+    private String username;
+    private String displayName;
 
 
-    private static final int PICK_IMAGE_REQUEST = 1;
+    private ActivityResultLauncher<Intent> pickImageLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    Uri selectedImageUri = result.getData().getData();
+
+
+                    InputStream inputStream = null;
+                    try {
+                        inputStream = getContentResolver().openInputStream(selectedImageUri);
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+                    circleImageView.setImageDrawable(new OvalImageDrawable(bitmap));
+
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +71,13 @@ public class SignUpActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.passwordEditText);
         confirmEditText = findViewById(R.id.confirmEditText);
         displayEditText = findViewById(R.id.displayEditText);
-        ImageView circleImageView = findViewById(R.id.circleImageView);
-
-
+        circleImageView = findViewById(R.id.circleImageView);
 
         usernameEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    String username = usernameEditText.getText().toString().trim();
+                    username = usernameEditText.getText().toString().trim();
                     // Check if the username is empty
                     if (username.isEmpty()) {
                         usernameEditText.setBackgroundResource(R.drawable.input_failure);
@@ -50,10 +87,24 @@ public class SignUpActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // Initialize the popup layout
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_layout, null);
+
+        // Create the popup window
+        int width = WindowManager.LayoutParams.MATCH_PARENT;
+        int height = WindowManager.LayoutParams.WRAP_CONTENT;
+        PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
         passwordEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
+                if (hasFocus) {
+                    // Show the popup when the EditText gains focus
+                    popupWindow.showAtLocation(passwordEditText, Gravity.TOP, 0, 0);
+                } else {
+                    // Dismiss the popup when the EditText loses focus
+                    popupWindow.dismiss();
                     password = passwordEditText.getText().toString().trim();
                     // regular expression that asks for at least one digit and one capital letter, length between 8-16
                     boolean isValidPassword = password.matches("^(?=.*[A-Z])(?=.*\\d).{8,16}$");
@@ -63,15 +114,16 @@ public class SignUpActivity extends AppCompatActivity {
                         passwordEditText.setBackgroundResource(R.drawable.input_success);
                     }
                 }
-
             }
         });
+
+
         confirmEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    String confirm = confirmEditText.getText().toString().trim();
-                    if (!confirm.equals(password)) {
+                    confirm = confirmEditText.getText().toString().trim();
+                    if (!confirm.equals(password) || confirm.isEmpty()) {
                         confirmEditText.setBackgroundResource(R.drawable.input_failure);
                     } else {
                         confirmEditText.setBackgroundResource(R.drawable.input_success);
@@ -84,7 +136,7 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    String displayName = displayEditText.getText().toString().trim();
+                    displayName = displayEditText.getText().toString().trim();
                     if (displayName.isEmpty()) {
                         displayEditText.setBackgroundResource(R.drawable.input_failure);
                     } else {
@@ -94,27 +146,12 @@ public class SignUpActivity extends AppCompatActivity {
 
             }
         });
-//
-//        uploadButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//                startActivityForResult(intent, PICK_IMAGE_REQUEST);
-//            }
-//        });
-    }
+        circleImageView.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            pickImageLauncher.launch(intent);
+        });
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
-//            Uri selectedImageUri = data.getData();
-//
-//            // Display the selected image in the CircleImageView
-//            //circleImageView.setImageURI(selectedImageUri);
-//        }
-//    }
+    }
 }
 
 
