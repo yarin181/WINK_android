9b6053d4132b53daf3286e16b499ac6a1c93881e
yoavShargay <yoavshra@gmail.com>
2023-06-17 18:55:21 +0300

    adding the get chats

--- app/src/main/java/com/example/wink_android/adapters/ChatsListAdapter.java
+++ app/src/main/java/com/example/wink_android/adapters/ChatsListAdapter.java
@@ -67,7 +67,7 @@ public class ChatsListAdapter extends RecyclerView.Adapter<ChatsListAdapter.Chat
     public void onBindViewHolder(ChatViewHolder holder, int position) {
         if (chats !=null){
             final Chat current = chats.get(position);
-            holder.ivProfilePic.setImageDrawable(new OvalImageDrawable(Utilities.stringToBitmap(current.getOtherProfilePic())));
+//            holder.ivProfilePic.setImageDrawable(new OvalImageDrawable(Utilities.stringToBitmap(current.getOtherProfilePic())));
             holder.tvUsername.setText(current.getOtherUsername());
             holder.tvLastMessage.setText("last msg...");
             holder.tvDate.setText("3/4/2222");
