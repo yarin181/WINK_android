5b2cce0a8342a8c4d65ac3a1e726f38e4010de20
Mor <morhanania@gmail.com>
2023-06-23 15:27:40 +0300

    fix the enter in add contact

--- app/src/main/java/com/example/wink_android/activities/popupsActivities/AddUserActivity.java
+++ app/src/main/java/com/example/wink_android/activities/popupsActivities/AddUserActivity.java
@@ -2,11 +2,13 @@ package com.example.wink_android.activities.popupsActivities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.wink_android.databinding.ActivityAddUserBinding;
 
 public class AddUserActivity extends AppCompatActivity {
@@ -24,17 +26,32 @@ public class AddUserActivity extends AppCompatActivity {
         getWindow().setAttributes(params);
         resultIntent = new Intent();
 
-        binding.addBtn.setOnClickListener(view -> {
-            if (binding.usernameEt.getText().toString().length() > 0) {
-                resultIntent.putExtra("username", binding.usernameEt.getText().toString());
-                setResult(AddUserActivity.RESULT_OK, resultIntent);
-                this.finish();
-            } else {
-                (Toast.makeText(this, "Not a Valid username", Toast.LENGTH_LONG)).show();
+        binding.usernameEt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE || event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    addContact();
+                    return true; // Return true to indicate that the event has been handled
+                }
+                return false; // Return false if the event is not handled
             }
+        });
+
+
+        binding.addBtn.setOnClickListener(view -> {
+            addContact();
         });
         binding.closeBtn.setOnClickListener(view -> {
             finish();
         });
+    }
+    private void addContact() {
+        if (binding.usernameEt.getText().toString().length() > 0) {
+            resultIntent.putExtra("username", binding.usernameEt.getText().toString());
+            setResult(AddUserActivity.RESULT_OK, resultIntent);
+            this.finish();
+        } else {
+            (Toast.makeText(this, "Not a Valid username", Toast.LENGTH_LONG)).show();
+        }
     }
 }
